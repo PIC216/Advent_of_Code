028e62b28ef7eeac0c6307f08198886640e2bbe0
Phoebe <101755626+PIC216@users.noreply.github.com>
2023-12-16 20:27:13 +0000

    Update day_1.py

--- 2023/day_1.py
+++ 2023/day_1.py
@@ -6,9 +6,9 @@ from my_functions import first_and_last_number
 from my_functions import find_word_num_and_replace
 from my_functions import fix_string
 
-total = 0
 
 # Part 1
+total = 0
 with open("advent_1_input.txt") as advent_1_input:
     for line in advent_1_input:
         num = first_and_last_number(line)
@@ -17,9 +17,8 @@ with open("advent_1_input.txt") as advent_1_input:
 print(total)
 
 
-total = 0
-
 # Part 2
+total = 0
 with open("advent_1_input.txt") as advent_1_input:
     for line in advent_1_input:
         fixed_line = fix_string(line)
