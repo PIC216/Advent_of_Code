a6b69a04a3109f1f3e8828ca7668036420f92bc2
Phoebe <101755626+PIC216@users.noreply.github.com>
2023-12-16 20:36:22 +0000

    Update my_functions.py
    Added Day 2 functions

--- 2023/my_functions.py
+++ 2023/my_functions.py
@@ -3,17 +3,23 @@ Contains all functions I create to solve the problems on Advent of Code 2023.
 '''
 
 # Used in Day 1
-def first_and_last_number(a_string: str):
-    for char in a_string:
+def first_or_last_digit(string_with_num, first: bool = True):
+    if not first:
+        string_with_num = string_with_num[::-1]
+    for char in string_with_num:
         if char.isdigit():
-            first = char
+            num = char
             break
-    for char in a_string[::-1]:
-        if char.isdigit():
-            last = char
-            break
+    return num
+    
+
+# Used in Day 1
+def first_and_last_digit(string_with_num: str):
+    first = first_or_last_digit(string_with_num, first = True)
+    last = first_or_last_digit(string_with_num, first = False)
     first_and_last = f"{first}{last}"
     return int(first_and_last)
+
 
 # Used in Day 1
 def fix_string(a_string: str):
@@ -31,6 +37,7 @@ def fix_string(a_string: str):
         a_string = a_string.replace(original, fixed)
     return a_string
 
+
 # Used in Day 1
 def find_word_num_and_replace(a_string: str):
     digits = {
@@ -47,4 +54,29 @@ def find_word_num_and_replace(a_string: str):
     for word, num in digits.items():
         a_string = a_string.replace(word, num)
     return a_string
-  
+
+
+# Used in Day 2
+def find_first_num(string_with_num):
+    num = int(re.findall(r'\d+', string_with_num)[0])
+    return num
+
+
+# Used in Day 2
+def get_game_num_and_sets(game_string):
+    game_num = find_first_num(game_string.split(": ")[0])
+    game_sets_ = game_string.split(": ")[1].split("; ")
+    game_sets = []
+    for game_set_ in game_sets_:
+        game_set = game_set_.split(", ")
+        game_sets.append(game_set)
+    return game_num, game_sets
+
+
+# Used in Day 2
+def update_max_in_colour_dict(colour_dict: dict = None, colour: str = "", a_string: str = ""):
+    if colour in a_string:
+        num = find_first_num(a_string)
+        if num > colour_dict[colour]:
+            colour_dict[colour] = num
+            
